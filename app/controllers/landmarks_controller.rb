2b7de14f63e713feb8db332833b9e6001911d598
Allyson Wesman <allysonwesman@gmail.com>
2018-02-28 12:09:07 -0800

    Done

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -36,9 +36,7 @@ end
 
 patch '/landmarks/:id' do
   @landmark = Landmark.find(params[:id])
-
-  @landmark.name = params[:landmark][:name]
-  @landmark.year_completed = params[:landmark][:year_completed]
+  @landmark.update(params[:landmark])
 
   @landmark.save
   redirect to "landmarks/#{@landmark.id}"
